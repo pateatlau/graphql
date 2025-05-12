65345bf2cafa30e260a3ba6bb40781719e8081af
Patea Tlau <pateatlau@gmail.com>
2025-05-12 23:20:27 +0530

    Minor changes in Queries.ts

--- frontend-react/src/GraphQL/Queries.ts
+++ frontend-react/src/GraphQL/Queries.ts
@@ -13,31 +13,3 @@ export const LOAD_USERS = gql`
     }
   }
 `;
-
-export const CREATE_USER = gql`
-  mutation createUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $gender: String!
-    $ipAddress: String!
-    $password: String
-  ) {
-    createUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      gender: $gender
-      ipAddress: $ipAddress
-      password: $password
-    ) {
-      id
-      firstName
-      lastName
-      email
-      gender
-      ipAddress
-      password
-    }
-  }
-`;
